ecd2deda2e0eb42b0c80e4d315f72d468f4c59ef
Vali <Vali_Schagerl@web.de>
2017-06-28 13:54:38 +0200

    formatData finished (?)

--- webserver/formatData.js
+++ webserver/formatData.js
@@ -1,46 +1,62 @@
-  var pg = require('pg');
-  var client = new pg.Client({
-      user: 'postgres',
-      password: 'password',
-      database: 'postgres',
-      host: 'localhost',
-  });
+var pg = require("pg");
 
-client.connect(function(err) {
-    if(err) {
-      res.status(500).send('could not connect to postgres');
-      return console.error('could not connect to postgres', err);
+var client = new pg.Client({
+  user: "postgres",
+  password: "password",
+  database: "postgres",
+  host: "localhost"
+});
+
+client.connect(function (err) {
+
+  if (err) {
+    res.status(500).send("could not connect to postgres");
+    return console.error("could not connect to postgres", err);
+  }
+
+  var query1 = "SELECT * FROM train_data WHERE device_id = 'rssi_1';";
+  var query2 = "SELECT * FROM train_data WHERE device_id = 'rssi_2';";
+  var query3 = "SELECT * FROM train_data WHERE device_id = 'rssi_3';";
+
+  var result1, result2, result3;
+
+  client.query(query1, function (err, result) {
+
+    if (err) {
+      return console.error("error running query", err);
     }
-    //new code, which only updates the one row; only the appropriate (depending on the device) is updated
-    var query1 = 'SELECT * FROM train_data WHERE device_id = \'rssi_1\';'
 
-    var query2 = 'SELECT * FROM train_data WHERE device_id = \'rssi_2\';'
+    result1 = result.rows;
 
-    var query3 = 'SELECT * FROM train_data WHERE device_id = \'rssi_3\';'
-
-    var result1, result2, result3;
-
-
-    client.query(query1, function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      result1=result.rows
     //   client.end();
-});
-    client.query(query2, function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
+
+    client.query(query2, function (err, result) {
+
+      if (err) {
+        return console.error("error running query", err);
       }
-      result2=result.rows
-    //   client.end();
-});
-    client.query(query3, function(err, result) {
-      if(err) {
-        return console.error('error running query', err);
-      }
-      result3=result.rows
-    //   client.end();
-});
-    console.log(result1,result2,result3);
+
+      result2 = result.rows;
+
+      client.query(query3, function (err, result) {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        result3 = result.rows;
+        console.log(result1, result2, result3);
+        for (var i = 0; i < 22; i++) {
+          var insertQuery = "INSERT INTO train_data_formatted (rssi1, rssi2, rssi3, location) VALUES (" + result1[i].rssi + ", " + result2[i].rssi + ", " + result3[i].rssi + ", " + result1[i].location +")";
+          client.query(insertQuery, function(err, result){
+            if(err){
+              return console.error("error running insertion inside for loop", err);
+            }else{
+              console.log("Inserted inside for loop");
+            }
+          });
+        }
+        client.end();        
+      });
+      //   client.end();
+    });
   });
+});
